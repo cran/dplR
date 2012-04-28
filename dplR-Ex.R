a1f59995aefdfd757d7317417f04d99b30e434dd
Andrew G. Bunn <andy.bunn@wwu.edu>
2012-04-28 05:16:43 +0000

    version 1.5.4

--- dplR-Ex.R
+++ dplR-Ex.R
@@ -503,12 +503,13 @@ flush(stderr()); flush(stdout())
 
 ### Name: read.ids
 ### Title: Read Site-Tree-Core IDs
-### Aliases: read.ids
+### Aliases: read.ids autoread.ids
 ### Keywords: misc
 
 ### ** Examples
 data(ca533)
 read.ids(ca533, stc = c(3, 2, 3))
+autoread.ids(ca533)
 
 
 
